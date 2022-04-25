387fc5d4315b52e6d2e163ea728d4cc69a1b0c4a
Dima <90210701+kiramurdaasmr@users.noreply.github.com>
2022-04-25 14:25:19 +0300

    25.04

---
+++ freecodecamp/js/55.js
@@ -0,0 +1,22 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true  
+    },
+  {
+     "artist":"me",
+      "title":"for you",
+      "release_year": 2022,
+      "formats":[
+        "cd",
+        "waw",
+        "cassette"]
+  }
+        ];
